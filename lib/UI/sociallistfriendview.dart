217f015a694e004a404b62dbb60fc61233608abd
Thanhtan101103 <161003191+baptt123@users.noreply.github.com>
2024-10-15 16:49:51 +0700

    cấu trúc code:thêm UI listfriend ở phần mạng xã hội(15/10/2024)

---
+++ lib/UI/sociallistfriendview.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Sociallistfriendview extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
+
+class SociallistfriendviewState extends State<Sociallistfriendview> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
